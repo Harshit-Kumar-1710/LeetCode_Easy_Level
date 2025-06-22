9440b2a31abd2ee7221e4d4984d17fcdbc9068c6
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-06-22 11:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,13 +3,9 @@ public:
     int fib(int n) 
     {
         int fib1=0,fib2=1;
-        if(n==1)
+        if(n<=1)
         {
-            return 1;
-        }
-        else if(n==0)
-        {
-            return 0;
+            return n;
         }
         for(int i=2;i<=n;i++)
         {
