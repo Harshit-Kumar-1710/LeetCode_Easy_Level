35d2c2a8727badd2a399a566fe432904b9448037
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-07-09 09:12:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (20.06%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.cpp
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
+    int SumofDigits(int n)
+    {
+        int sum1=0;
+        while(n>0)
+        {
+            int Rem=n%10;
+            sum1+=Rem;
+            n=n/10;
+        }
+    return sum1;
+    }
     int differenceOfSum(vector<int>& nums) 
     {
-        int sum=0;
+        int sum=0,sum1=0;
         for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
+            sum1+=SumofDigits(nums[i]);
         }
-        int sum1=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            int n=nums[i];
-            while(n>0)
-            {
-                int Rem=n%10;
-                sum1+=Rem;
-                n=n/10;
-            }
-        }   
     return abs(sum-sum1);
     }
 };
