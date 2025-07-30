750f1243493aee05ecd0858cd141f104bac3d8a9
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-07-30 13:54:08 +0530

    Time: 8 ms (16.34%) | Memory: 13.5 MB (51.98%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -6,10 +6,8 @@ public:
         int i=0,j=n,n1=nums.size();
         while(j<n1)
         {
-            Vec.push_back(nums[i]);
-            Vec.push_back(nums[j]);
-            i++;
-            j++;
+            Vec.push_back(nums[i++]);
+            Vec.push_back(nums[j++]);
         }
     return Vec;
     }
