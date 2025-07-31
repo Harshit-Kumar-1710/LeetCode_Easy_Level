8a4d0341c00f5b1bc71dbdf2dd6782878ab193b2
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-07-31 12:17:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (79.58%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> runningSum(vector<int>& nums) 
     {
         int n=nums.size();
-        cout<<"G_nd_ mera code dekh raha eske liye bhi";
         for(int i=1;i<n;i++)
         {
             nums[i]=nums[i-1]+nums[i];
