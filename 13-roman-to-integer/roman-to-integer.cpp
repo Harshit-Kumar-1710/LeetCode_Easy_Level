eeb376fc791619152a5cbec6a11993321297d80b
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-08-01 17:49:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (77.25%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,44 +1,55 @@
 class Solution {
 public:
-    int Func(char c,map<char,int>&m)
+    int Func(char c)
     {
-       for(auto p:m)
-       {
-            if(p.first==c)
-            {
-                return p.second;
-            }
-       }
-    return -1;
+        if(c=='I')
+        {
+            return 1;
+        }
+        else if(c=='V')
+        {
+            return 5;
+        }
+        else if(c=='X')
+        {
+            return 10;
+        }
+        else if(c=='L')
+        {
+            return 50;
+        }
+        else if(c=='C')
+        {
+            return 100;
+        }
+        else if(c=='D')
+        {
+            return 500;
+        }
+        else
+        {
+            return 1000;
+        }
     }
     int romanToInt(string s) 
     {
         int n=s.size(),i=0,Num=0;
-        map<char,int>m;
-            m['I']=1;
-            m['V']=5;
-            m['X']=10;
-            m['L']=50;
-            m['C']=100;
-            m['D']=500;
-            m['M']=1000;
-            i=0;
-            while(i<n)
+        while(i<n)
+        {
+            if(i==n-1)
             {
-                if(i==n-1)
-                {
-                    Num+=Func(s[i],m);
-                }
-                else if(Func(s[i],m)>=Func(s[i+1],m))
-                {
-                    Num+=Func(s[i],m);
-                }
-                else
-                {
-                    Num-=Func(s[i],m);
-                }
-                i++;
+                Num+=Func(s[i]);
             }
+            else if(Func(s[i])>=Func(s[i+1]))
+            {
+                Num+=Func(s[i]);
+            }
+            else
+            {
+                Num-=Func(s[i]);
+            }
+            i++;
+        }
     return Num;
     }
 };
