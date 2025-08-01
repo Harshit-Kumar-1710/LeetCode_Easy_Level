6d2f3a8589c49aba2dec22e04b51fee15053c028
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-08-01 17:45:52 +0530

    Time: 6 ms (55.92%) | Memory: 11.7 MB (72.19%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,55 +1,68 @@
 class Solution {
 public:
-    int Func(char c)
+    int Func(char c,map<char,int>&m)
     {
-        if(c=='I')
-        {
-            return 1;
-        }
-        else if(c=='V')
-        {
-            return 5;
-        }
-        else if(c=='X')
-        {
-            return 10;
-        }
-        else if(c=='L')
-        {
-            return 50;
-        }
-        else if(c=='C')
-        {
-            return 100;
-        }
-        else if(c=='D')
-        {
-            return 500;
-        }
-        else
-        {
-            return 1000;
-        }
+       for(auto p:m)
+       {
+            if(p.first==c)
+            {
+                return p.second;
+            }
+       }
+    return -1;
     }
     int romanToInt(string s) 
     {
         int n=s.size(),i=0,Num=0;
-        while(i<n)
+        map<char,int>m;
+        for(int i=0;i<n;i++)
         {
-            if(i==n-1)
+            if(s[i]=='I')
             {
-                Num+=Func(s[i]);
+                m[s[i]]=1;
             }
-            else if(Func(s[i])>=Func(s[i+1]))
+            else if(s[i]=='V')
             {
-                Num+=Func(s[i]);
+                m[s[i]]=5;
+            }
+            else if(s[i]=='X')
+            {
+                m[s[i]]=10;
+            }
+            else if(s[i]=='L')
+            {
+                m[s[i]]=50;
+            }
+            else if(s[i]=='C')
+            {
+                m[s[i]]=100;
+            }
+            else if(s[i]=='D')
+            {
+                m[s[i]]=500;
             }
             else
             {
-                Num-=Func(s[i]);
+                m[s[i]]=1000;
             }
-            i++;
         }
+            i=0;
+            while(i<n)
+            {
+                if(i==n-1)
+                {
+                    Num+=Func(s[i],m);
+                }
+                else if(Func(s[i],m)>=Func(s[i+1],m))
+                {
+                    Num+=Func(s[i],m);
+                }
+                else
+                {
+                    Num-=Func(s[i],m);
+                }
+                i++;
+            }
     return Num;
     }
 };
