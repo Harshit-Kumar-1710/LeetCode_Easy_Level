7e818e28de28d2c92e9f41f71454f937c50ae45e
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-08-03 18:56:52 +0530

    Time: 3 ms (6.94%) | Memory: 10 MB (5.56%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,103 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        int n=s.size();
+        stack<int>S;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+            {
+                S.push(s[i]);
+            }
+            else
+            {
+                if(S.empty())
+                {
+                    return false;
+                }
+                else
+                {
+                    if(s[i]==')')
+                    {
+                        if(S.top()=='(')
+                        {
+                            S.pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if(s[i]=='}')
+                    {
+                        if(S.top()=='{')
+                        {
+                            S.pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if(S.top()=='[')
+                        {
+                            S.pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        if(S.size()!=0)
+        {
+            return false;
+        }
+    return true;
+    }
+};
+                /*if(S.top()=='(')
+                {
+                    S.pop();
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+            else if(s[i]=='}')
+            {
+                if(S.top()=='{')
+                {
+                    S.pop();
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if(S.top()==']')
+                {
+                    S.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        if(S.size()!=0)
+        {
+            return false;
+        }
+    return true;
+        
+    }
+};*/
