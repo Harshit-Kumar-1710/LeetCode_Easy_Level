9f15160ea590c3ae9879cf8393b47fb9a1769cf7
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-08-08 10:54:26 +0530

    Time: 4 ms (21.56%) | Memory: 86.2 MB (38.03%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
@@ -4,16 +4,12 @@ public:
     {
         int n=nums.size(),Maxi=0;
         for(int i=0;i<n;i++)
-        {
             Maxi=max(Maxi,nums[i]);
-        }
         int Ans=1;
         for(int i=0;i<n;i++)
         {
             if(nums[i]!=Maxi)
-            {
                 continue;
-            }
             i++;
             int Current=1;
             while(i<n && nums[i]==nums[i-1])
