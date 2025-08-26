099f32f31cb864de7aa5100127602a2eec380db9
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-08-26 22:47:27 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (92.23%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,20 +4,16 @@ public:
     {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>>P(n,vector<int>(m,-1));
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-1;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=i+1;j<m;j++)
             {
-                P[j][m-i-1]=matrix[i][j];
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        for(int i=0;i<(int)P.size();i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<(int)P[0].size();j++)
-            {
-                matrix[i][j]=P[i][j];
-            }
+            reverse(matrix[i].begin(),matrix[i].end());
         }
     }
 };
